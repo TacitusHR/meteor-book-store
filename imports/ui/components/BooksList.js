97df6c41116d479cd9fe0b566234ff786757c435
Berislav <berislav.hunjadi@gmail.com>
2018-02-10 09:48:32 +0100

    Trying to configure eslint - wip

--- imports/ui/components/BooksList.js
+++ imports/ui/components/BooksList.js
@@ -1,7 +1,40 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {withTracker} from 'meteor/react-meteor-data';
+import Books from '../../api/books/books';
 
-export default class BooksList extends Component {
+// export class BookItem extends Component {
+//     static propTypes = {
+//         book: PropTypes.object.isRequired,
+//     };
+//
+//     render() {
+//         const {title, author} = this.props.book;
+//         return (
+//             <li className="list-group-item">
+//                 <strong>{title}</strong> &mdash; by {author}
+//             </li>
+//         );
+//     }
+// }
+
+export function BookItem({book: {title, author}}) {
+    return (
+        <li className="list-group-item">
+            <strong>{title}</strong> &mdash; by {author}
+        </li>
+    );
+}
+
+BookItem.propTypes = {
+    book: PropTypes.object.isRequired,
+};
+
+export function NoBooks() {
+    return (<p className="alert alert-warning">No books yet, hombre.</p>);
+}
+
+export class BooksList extends Component {
     static propTypes = {
         books: PropTypes.array,
     };
@@ -11,8 +44,21 @@ export default class BooksList extends Component {
     };
 
     render() {
+        const {books} = this.props;
+        if (books.length === 0) {
+            return (<NoBooks />);
+        }
+
         return (
-            <ul className="list-group" />
+            <ul className="list-group">
+                {books.map((book, index) => <BookItem key={index} book={book} />)}
+            </ul>
         );
     }
 }
+
+export default withTracker(() => {
+    return {
+        books: Books.find().fetch(),
+    };
+})(BooksList);
